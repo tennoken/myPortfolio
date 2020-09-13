2205f2713c2a050721b5a7bd7ee5067b5c3f6502
Kzero <41933570+tennoken@users.noreply.github.com>
2020-09-13 20:40:26 +0900

    JS : Arrow up button

--- main.js
+++ main.js
@@ -32,9 +32,21 @@ navbarMenu.addEventListener('click', (event) => {
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => scrollToSection('#contact'));
 
-//
+//home 에서 스크롤을 내렸을 때 home 안에 내용이 점점 사라짐
 const homeContainer = document.querySelector('.home__container');
 const homeHeight = homeContainer.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// 스크롤이 homeHeight의 반으로 내려갔을 때 arrow--btn이 생김;
+const arrowBtn = document.querySelector('.arrow--btn');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowBtn.classList.add('visible');
+  } else {
+    arrowBtn.classList.remove('visible');
+  }
+});
+// arrowBtn 클릭시 home으로 스크롤
+arrowBtn.addEventListener('click', () => scrollToSection('#home'));
