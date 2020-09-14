9bace8b0d4540de1b26751227b973d5d8d52c965
Kzero <41933570+tennoken@users.noreply.github.com>
2020-09-14 21:48:39 +0900

    JS : Project Filter

--- main.js
+++ main.js
@@ -21,10 +21,20 @@ document.addEventListener('scroll', () => {
 
 // navbar 메뉴 클릭 시 해당 section으로 이동
 const navbarMenu = document.querySelector('.navbar__menu');
+const navarMenuItem = document.querySelectorAll('.navbar__menu__item');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) return;
+
+  // navarMenuItem.forEach((item) => {
+  //   if (item === event.target) {
+  //     item.classList.add('active');
+  //   } else {
+  //     item.classList.remove('active');
+  //   }
+  // });
+
   scrollToSection(link);
 });
 
@@ -50,3 +60,35 @@ document.addEventListener('scroll', () => {
 });
 // arrowBtn 클릭시 home으로 스크롤
 arrowBtn.addEventListener('click', () => scrollToSection('#home'));
+
+// work에 버튼을 클릭시 프로젝트 필터
+const workBtnContainer = document.querySelector('.work__categories');
+const projectsContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+const categoryBtn = document.querySelectorAll('.category__btn');
+
+workBtnContainer.addEventListener('click', (event) => {
+  const filter =
+    event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) return;
+
+  categoryBtn.forEach((btn) => {
+    if (btn === event.target) {
+      btn.classList.add('active');
+    } else {
+      btn.classList.remove('active');
+    }
+  });
+
+  projectsContainer.classList.add('animation--out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === 'all' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectsContainer.classList.remove('animation--out');
+  }, 300);
+});
